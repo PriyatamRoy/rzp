4fcc1962a82e19ab131024a4b08801bb6650f4ce
Priyatam Roy <Priyatam.Roy@wdc.com>
2018-10-25 18:48:49 +0530

    adding env port

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.post('/payment-authorized', (req, res) => {
 	
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server is listening on port 3000");
 });
 
